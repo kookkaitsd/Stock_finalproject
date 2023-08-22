0cff5e068981203573e325e8386f28f49e71b489
twinkykookkai <thansuda0601@gmail.com>
2023-08-22 23:23:00 +0700

    first commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,15 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.1.0
+    </div>
+    <strong>ToB1 Hair Station  2023-<?php echo date('Y') ?>.</strong> 
+  </footer>
+
+ 
+  <div class="control-sidebar-bg"></div>
+</div>
+
+
+</body>
+</html>
---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,16 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="#" class="logo">
+    <img src="icon.png" width="50" height="50" />
+    </a>
+   
+    <nav class="navbar navbar-static-top">
+     
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+    </nav>
+  </header>
+
+  
